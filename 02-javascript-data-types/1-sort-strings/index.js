c1cbb171136827a272b5c6eb54e09ec6142562fb
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2021-01-31 13:19:42 +0500

    02-tasks decisions

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,25 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    let arrCopy = [];
+    
+    for (let item of arr) { 
+        arrCopy.push(item);
+    }
 
+    if (param === 'asc') { 
+        arrCopy.sort((a, b) => {
+            if (a[0].toLowerCase() === b[0].toLowerCase() && a[0] !== b[0]) return b.localeCompare(a);
+            return a.localeCompare(b);
+        });
+    }
+
+    if (param === 'desc') { 
+        arrCopy.sort((a, b) => {
+            if (a[0].toLowerCase() === b[0].toLowerCase() && a[0] !== b[0]) return a.localeCompare(b);
+            return b.localeCompare(a);
+        });
+    }
+
+    return arrCopy;
 }
--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,14 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+    const arrKeysValues = Object.entries(obj);
+    const resultObj = {};
+    
+    for (let item of fields) { 
+        arrKeysValues.forEach(element => {
+            if (element[0] === item) resultObj[item] = element[1];
+        });
+    }; 
 
+    return resultObj;
 };
