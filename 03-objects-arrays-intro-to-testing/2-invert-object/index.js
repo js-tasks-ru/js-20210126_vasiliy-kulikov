896726d1e2616829804f56a168596dd60317d8c5
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2021-02-05 01:48:48 +0500

    03 tasks

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,8 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    if (!obj) return obj;
+    let result = {};
+    Object.entries(obj).forEach(item => result[item[1]] = item[0]);
+    return result;
 }
