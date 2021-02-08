9bf35297b85ca2b7e00ec57ad17c8fea95ae8b6e
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2021-02-09 01:12:14 +0500

    04 begin

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,25 @@
 export default class ColumnChart {
+    constructor({data, label, value, ...rest}) { 
+        this.data = data;
+        this.label = label;
+        this.value = value;
+    }
 
+    render() { 
+        let div 
+        let targetContainer = document.body.querySelector(`#${this.label}`);
+        /*
+        switch (this.label) { 
+            case 'orders':
+                targetContainer = document.body.querySelector('')
+                break;
+            case 'sales':
+                break;
+            case 'customers':
+                break;
+        }
+        */
+        
+
+    }
 }
