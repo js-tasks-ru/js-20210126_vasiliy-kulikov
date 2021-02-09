689ea201c916d7074b8bdf999c8a0bb41cd5ac0b
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2021-02-09 20:55:22 +0500

    04 decision

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,25 +1,64 @@
 export default class ColumnChart {
-    constructor({data, label, value, ...rest}) { 
+    constructor({ data = [], label, value, link, chartHeight = 50 } = {}) { 
         this.data = data;
         this.label = label;
         this.value = value;
+        this.link = link;
+        this.chartHeight = chartHeight;
+
+        this.render();
     }
 
     render() { 
-        let div 
-        let targetContainer = document.body.querySelector(`#${this.label}`);
-        /*
-        switch (this.label) { 
-            case 'orders':
-                targetContainer = document.body.querySelector('')
-                break;
-            case 'sales':
-                break;
-            case 'customers':
-                break;
-        }
-        */
-        
 
+        const chart = document.createElement('div');
+        chart.classList.add('column-chart');
+        chart.style.cssText = `--chart-height: ${this.chartHeight}`;
+
+        let chartData = this.makeDataRightValue();
+
+        chartData = chartData.map((item) => {
+            return `<div style="--value: ${Math.floor(item)}" data-tooltip="${Math.round(item / this.chartHeight * 100)}%"></div>`
+        });
+
+        chart.innerHTML = `
+            <div class="column-chart__title">
+                    Total ${this.label}
+                    <a href="${this.link}" class="column-chart__link">View all</a>
+            </div>
+            <div class="column-chart__container">
+                <div data-element="header" class="column-chart__header">${this.value}</div>
+                <div data-element="body" class="column-chart__chart">${chartData.join('')}</div>
+            </div>
+        `;
+
+        if (this.data.length === 0) {
+            chart.classList.add('column-chart_loading');
+        }
+            
+        this.element = chart;
+    }
+
+    makeDataRightValue() { 
+        let coefValues, newChartData;
+        let maxValue = Math.max(...this.data);
+
+        coefValues = this.chartHeight / maxValue;
+        newChartData = this.data.map(item => item * coefValues);
+
+        return newChartData;
+    }
+
+    update(data) {
+        this.data = data;
+        this.render();
+    }
+
+    destroy() { 
+
+    }
+
+    remove() {
+        this.element.remove();
     }
 }
