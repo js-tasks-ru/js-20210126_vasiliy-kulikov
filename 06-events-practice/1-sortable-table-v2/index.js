081b86eda4c40538f192928cee69bb9b7a7fd7bc
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2021-02-17 01:17:40 +0500

    06 during

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -14,6 +14,27 @@ export default class SortableTable {
         this.element = table;
 
         this.subElements = this.getSubElements(table.firstElementChild);
+
+        this.subElements['header'].addEventListener('click', evt => {
+            const parent = evt.target.closest('.sortable-table__cell');
+            const column = parent.dataset.id;
+            
+            let sortingOrder;
+
+            switch (parent.dataset.order) { 
+                case 'asc':
+                    sortingOrder = 'desc';
+                    break;
+                case 'desc':
+                    sortingOrder = 'asc';
+                    break;
+                default:
+                    sortingOrder = 'asc';
+                    break;
+            }
+
+            this.sort(column, sortingOrder);
+        });
     }
 
     getTable() {     
@@ -36,7 +57,7 @@ export default class SortableTable {
     getTemplateBody() {  
         return `
             <div data-element="body" class="sortable-table__body">
-                ${this.getTemplateBodyRows().join('')}
+                ${this.getTemplateBodyRows(this.data).join('')}
             </div>  
         ` 
     }
@@ -52,8 +73,8 @@ export default class SortableTable {
         });          
     }
 
-    getTemplateBodyRows() {
-        return this.data.map(item => {
+    getTemplateBodyRows(data) {
+        return data.map(item => {
             return `
                 <a href="/products/${item.id}" class="sortable-table__row">
                     ${this.getTemplateBodyRowCells(item).join('')}
@@ -91,6 +112,41 @@ export default class SortableTable {
             accum[subElement.dataset.element] = subElement;
             return accum;
         }, {});
+    }
+
+    sort(field, order) { 
+        const sortedData = this.sortData(field, order);
+
+        const allColumns = this.element.querySelectorAll('.sortable-table__cell[data-id]');
+        const currentColumn = this.element.querySelector(`.sortable-table__cell[data-id = ${field}]`);
+
+        allColumns.forEach(item => {
+            item.dataset.order = '';
+        });
+
+        currentColumn.dataset.order = order;
+
+        this.subElements.body.innerHTML = this.getTemplateBodyRows(sortedData);
+    }
+
+    sortData(field, order) { 
+        const arr = [...this.data];
+        const column = this.header.find(item => item.id === field);
+        const { sortType, customSorting } = column;
+        const direction = order === 'asc' ? 1 : -1;
+
+        return arr.sort((a, b) => {
+            switch (sortType) {
+                case 'number':
+                    return direction * (a[field] - b[field]);
+                case 'string':
+                    return direction * a[field].localeCompare(b[field], ['ru', 'en'], { caseFirst: 'upper' });
+                case 'custom':
+                    return direction * customSorting(a, b);
+                default:
+                    return direction * (a[field] - b[field]);
+            }
+        });
     }
 
     destroy() { 
