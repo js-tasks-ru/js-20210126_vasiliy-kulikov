a424c35127cdde61239220766c52c9bee3c840bf
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2021-02-16 21:43:33 +0500

    06 begin

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,3 +1,99 @@
 export default class SortableTable {
+    constructor(header, { data } = {}) { 
+        this.header = header;
+        this.data = data;
+        this.render();
+    }
 
+    render() { 
+        const table = document.createElement('div');
+        table.className = 'products-list__container';
+        table.dataset.element = 'productsContainer';
+        table.innerHTML = this.getTable();
+        
+        this.element = table;
+
+        this.subElements = this.getSubElements(table.firstElementChild);
+    }
+
+    getTable() {     
+        return `
+            <div class="sortable-table">
+                ${this.getTableHeader()}
+                ${this.getTemplateBody()}                  
+            </div>
+        `;
+    }
+
+    getTableHeader() { 
+        return `
+            <div data-element="header" class="sortable-table__header sortable-table__row">
+                ${this.getTableHeaderRow().join('')}
+            </div>
+        `
+    }
+
+    getTemplateBody() {  
+        return `
+            <div data-element="body" class="sortable-table__body">
+                ${this.getTemplateBodyRows().join('')}
+            </div>  
+        ` 
+    }
+
+    getTableHeaderRow() { 
+        return this.header.map(item => {
+            return `
+                <div class="sortable-table__cell" data-id="${item.id}" data-sortable="${item.sortable}" data-order="">
+                    <span>${item.title}</span> 
+                    ${this.getHeaderArrow()}
+                </div>
+            `
+        });          
+    }
+
+    getTemplateBodyRows() {
+        return this.data.map(item => {
+            return `
+                <a href="/products/${item.id}" class="sortable-table__row">
+                    ${this.getTemplateBodyRowCells(item).join('')}
+                </a>
+            `
+        })
+    }
+    
+    getTemplateBodyRowCells(item) {
+        const arrayCells = [];
+
+        for (let cell of this.header) {
+            if (cell.template) {
+                arrayCells.push(cell.template(item[cell.id]));
+            } else {
+                arrayCells.push(`<div class="sortable-table__cell">${item[cell.id]}</div>`);
+            }
+        }
+
+        return arrayCells;
+    }  
+    
+    getHeaderArrow() { 
+        return `
+            <span data-element="arrow" class="sortable-table__sort-arrow">
+                <span class="sort-arrow"></span>
+            </span>
+        `
+    }
+
+    getSubElements(element) { 
+        const elements = element.querySelectorAll('[data-element]');
+        
+        return [...elements].reduce((accum, subElement) => {
+            accum[subElement.dataset.element] = subElement;
+            return accum;
+        }, {});
+    }
+
+    destroy() { 
+        this.element.remove();
+    }
 }
