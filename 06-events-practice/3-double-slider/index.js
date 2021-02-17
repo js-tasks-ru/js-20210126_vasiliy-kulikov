5e0189450b8f6afe3c77a12eaadd2294c74b179f
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2021-02-17 18:49:02 +0500

    06-03 begin

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -1,3 +1,41 @@
 export default class DoubleSlider {
+    constructor() { 
+        this.render();
+    }
 
+    getTemplate() { 
+        return `
+            <div class="range-slider">
+                <span>$100</span>
+                    <div class="range-slider__inner">
+                        <span class="range-slider__progress"></span>
+                        <span class="range-slider__thumb-left"></span>
+                        <span class="range-slider__thumb-right"></span>
+                    </div>
+                <span>$100</span>
+            </div>
+        `
+    }
+
+    render() { 
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = this.getTemplate();
+
+        this.element = wrapper.firstElementChild;
+
+        this.element.addEventListener('pointerdown', (evt) => {
+            if (evt.target.className !== 'range-slider__thumb-left' &&
+                evt.target.className !== 'range-slider__thumb-right') return;
+            
+            this.move(evt);
+        });
+    }
+
+    move(evt) { 
+        
+    }
+
+    /*let event = new Event('range-select', {
+
+    })*/
 }
