04b619529f107a0dd814c39aefff8e15b72d81ab
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2021-02-19 22:44:02 +0500

    06-3 making

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -1,20 +1,13 @@
 export default class DoubleSlider {
-    constructor() { 
-        this.render();
-    }
+    constructor(sliderConfig = {}) { 
+        this.min = sliderConfig.min;
+        this.max = sliderConfig.max;
+        this.formatValue = sliderConfig.formatValue;
+        this.selected = sliderConfig.selected;
 
-    getTemplate() { 
-        return `
-            <div class="range-slider">
-                <span>$100</span>
-                    <div class="range-slider__inner">
-                        <span class="range-slider__progress"></span>
-                        <span class="range-slider__thumb-left"></span>
-                        <span class="range-slider__thumb-right"></span>
-                    </div>
-                <span>$100</span>
-            </div>
-        `
+        this.sliderConfig = sliderConfig;
+
+        this.render();
     }
 
     render() { 
@@ -28,35 +21,67 @@ export default class DoubleSlider {
         this.initialization();
     }
 
+    getTemplate() { 
+        let valueMin, valueMax;
+
+        if (this.sliderConfig.min) valueMin = this.sliderConfig.min;
+
+        if (this.sliderConfig.max) valueMax = this.sliderConfig.max;
+
+        return `
+            <div class="range-slider">
+                <span data-element='from'>${this.sliderConfig.formatValue(valueMin)}</span>
+                    <div class="range-slider__inner">
+                        <span class="range-slider__progress"></span>
+                        <span class="range-slider__thumb-left"></span>
+                        <span class="range-slider__thumb-right"></span>
+                    </div>
+                <span data-element='to'>${this.sliderConfig.formatValue(valueMax)}</span>
+            </div>
+        `
+    }
+
     initialization() { 
         this.leftThumb = this.element.querySelector('.range-slider__thumb-left');
+        this.leftBoundry = this.element.querySelector('span[data-element = "from"]')
+
         this.rightThumb = this.element.querySelector('.range-slider__thumb-right');
+        this.rightBoundry = this.element.querySelector('span[data-element = "to"]');
+
+        this.sliderLine = this.element.querySelector('.range-slider__inner');
         this.progressLine = this.element.querySelector('.range-slider__progress');
+
+        this.sliderScaleRange = this.max - this.min;
+
+        if (this.sliderConfig.selected) { 
+            this.leftBoundry.textContent = this.formatValue(this.sliderConfig.selected.from);
+            this.rightBoundry.textContent = this.formatValue(this.sliderConfig.selected.to);
+
+            this.progressLine.style.left = (this.sliderConfig.selected.from - this.min) / this.sliderScaleRange * 100 + '%';
+            this.leftThumb.style.left = this.progressLine.style.left;
+        }
 
         this.element.addEventListener('pointerdown', this.pointerDown);
 
     }
 
     pointerDown = (evt) => {
-        
 
         if (evt.target !== this.leftThumb && evt.target !== this.rightThumb) return;
 
-        this.progressLine.width = this.progressLine.getBoundingClientRect().width;
-        //console.log(this.progressLine.width);
+        this.sliderLine.width = this.sliderLine.getBoundingClientRect().width;
         
         (evt.target === this.leftThumb) ? this.activeThumb = this.leftThumb : this.activeThumb = this.rightThumb;
 
         switch (this.activeThumb) { 
             
             case this.leftThumb:
-                console.log(this.leftThumb.positionStart);
+
                 if (!this.leftThumb.positionStart) { 
-                    
                     this.leftThumb.positionStart = this.leftThumb.getBoundingClientRect().left;
-                    this.leftThumb.shiftX = evt.clientX - this.leftThumb.positionStart;
-                    console.log(this.leftThumb.positionStart);
                 };     
+
+                this.leftThumb.ondragstart = () => false;
 
                 document.addEventListener('pointermove', this.moveLeftThumb);
                 break;
@@ -64,9 +89,9 @@ export default class DoubleSlider {
             case this.rightThumb:
                 if (!this.rightThumb.positionStart) { 
                     this.rightThumb.positionStart = this.rightThumb.getBoundingClientRect().right;
-                    this.rightThumb.shiftX = this.rightThumb.positionStart - evt.clientX;
                 };
 
+                this.rightThumb.ondragstart = () => false;
                 document.addEventListener('pointermove', this.moveRightThumb);
                 break;
             
@@ -76,50 +101,53 @@ export default class DoubleSlider {
     };
 
     moveLeftThumb = (evt) => {  
-        document.addEventListener('pointerup', this.pointerLeftUp);
 
-        if (evt.clientX < this.leftThumb.positionStart) {
-            this.leftThumb.style.cssText = `position: absolute; z-index: 10000; left: 0%`;
+        if ((evt.clientX <= this.leftThumb.positionStart) && (this.leftThumb.positionStart < this.sliderLine.getBoundingClientRect().left)) {
+            this.leftThumb.style.cssText = `z-index: 10000; left: 0%`;
+            this.leftBoundry.textContent = this.formatValue(this.min);
         } else { 
             this.leftThumb.style.cssText = `
-                position: absolute; z-index: 10000;
-                left: ${(evt.clientX - this.leftThumb.positionStart) / this.progressLine.width * 100}%
+                z-index: 10000;
+                left: ${(evt.clientX - this.leftThumb.positionStart) / this.sliderLine.width * 100}%
             `;
+            
+            this.leftBoundry.textContent = this.formatValue(Math.round(this.min + this.sliderScaleRange * (evt.clientX - this.leftThumb.positionStart) / this.sliderLine.width));
         }
 
         if (this.leftThumb.getBoundingClientRect().right >= this.rightThumb.getBoundingClientRect().left) { 
-
             this.leftThumb.style.cssText = `
-                position: absolute; z-index: 10000;
+                z-index: 10000;
                 left: ${100 - parseFloat(this.rightThumb.style.right)}%;
             `;
-            
         }
 
         this.progressLine.style.left = `${parseFloat(this.leftThumb.style.left)}%`;
+
+        document.addEventListener('pointerup', this.pointerLeftUp);
     }
 
     moveRightThumb = (evt) => { 
-        document.addEventListener('pointerup', this.pointerRightUp);
 
         if (evt.clientX > this.rightThumb.positionStart) {
-            this.rightThumb.style.cssText = `position: absolute; z-index: 10000; right: 0%`;
+            this.rightThumb.style.cssText = `z-index: 10000; right: 0%`;
         } else { 
             this.rightThumb.style.cssText = `
-                position: absolute; z-index: 10000;
-                right: ${(this.rightThumb.positionStart - evt.clientX - this.rightThumb.shiftX) / this.progressLine.width * 100}%
+                z-index: 10000;
+                right: ${(this.rightThumb.positionStart - evt.clientX) / this.sliderLine.width * 100}%
             `;
         }
         
         if (this.leftThumb.getBoundingClientRect().right >= this.rightThumb.getBoundingClientRect().left) { 
 
             this.rightThumb.style.cssText = `
-                position: absolute; z-index: 10000;
+                z-index: 10000;
                 right: ${100 - parseFloat(this.leftThumb.style.left)}%;
             `;  
         }
         
         this.progressLine.style.right = `${parseFloat(this.rightThumb.style.right)}%`;
+
+        document.addEventListener('pointerup', this.pointerRightUp);
     }
 
     pointerLeftUp = () => { 
@@ -132,32 +160,7 @@ export default class DoubleSlider {
         document.removeEventListener('pointerup', this.pointerRightUp);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    pointerMove = (evt) => { 
-        let activeThumbPositionPercentage = (evt.clientX - this.activeThumbLeftStart - this.shiftX) / this.sliderLineWidth * 100;
-        console.log(`Старт Мыши: ${evt.clientX} / Старт Тумбы: ${this.activeThumbLeftStart} / Шифт: ${this.shiftX}`);
-        this.activeThumb.style.left = `${activeThumbPositionPercentage}%`;
-
-        if (activeThumbPositionPercentage <= 0) this.activeThumb.style.left = '0%';
-
-        document.addEventListener('pointerup', this.pointerUp);
+    destroy() { 
+        this.element.remove();
     }
-
-    pointerUp = () => { 
-        document.removeEventListener('pointermove', this.pointerMove);
-        document.removeEventListener('pointerup', this.pointerUp);
-    }
-    */
 }
