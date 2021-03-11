3def5fe2f0d56a452ab7899e251f040b844048f6
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2021-03-12 01:34:05 +0500

    10 doing

--- 10-routes-browser-history-api/1-dashboard-page/index.js
+++ 10-routes-browser-history-api/1-dashboard-page/index.js
@@ -16,8 +16,35 @@ export default class Page {
     async render() {
         const wrapper = document.createElement('div');
         wrapper.className = 'dashboard full-height flex-column';
+        wrapper.innerHTML = this.getTemplate();
 
-        wrapper.innerHTML = `
+        this.getElements();
+
+        wrapper.querySelector('.content__top-panel').append(this.rangePicker.element);
+        wrapper.querySelector('.dashboard__charts').append(this.chartOrders.element);
+        wrapper.querySelector('.dashboard__charts').append(this.chartSales.element);
+        wrapper.querySelector('.dashboard__charts').append(this.chartCustomers.element);
+        wrapper.append(this.sortableTable.element);
+
+        this.element = wrapper;
+
+        // this.subElements = {
+        //     rangePicker: rangePicker.element,
+        //     sortableTable: sortableTable.element,
+        //     ordersChart: chartOrders.element,
+        //     customersChart: chartCustomers.element,
+        //     salesChart: chartSales.element 
+        // }
+
+        wrapper.remove();
+
+        this.initEventListeners();
+
+        return this.element;
+    }
+
+    getTemplate() {
+        return `
             <div class="content__top-panel">
                 <h2 class="page-title">Панель управления</h2>
             </div>
@@ -25,17 +52,19 @@ export default class Page {
             </div>
             <h3 class="block-title">Лидеры продаж</h3>
         `;
+    }
 
-        const rangePicker = new RangePicker({
+    getElements() {
+        this.rangePicker = new RangePicker({
             from: this.dates.from,
             to: this.dates.to
         });
         
-        const sortableTable = new SortableTable(header, {
-            url: 'api/rest/products'
+        this.sortableTable = new SortableTable(header, {
+            url: `api/dashboard/bestsellers?from=${this.dates.from.toISOString()}&to=${this.dates.to.toISOString()}`
         });
-        
-        const chartOrders = new ColumnChart({
+
+        this.chartOrders = new ColumnChart({
             url: 'api/dashboard/orders',
             range: {
                 from: this.dates.from,
@@ -45,7 +74,7 @@ export default class Page {
             link: '#'
         });
 
-        const chartSales = new ColumnChart({
+        this.chartSales = new ColumnChart({
             url: 'api/dashboard/sales',
             range: {
               from: this.dates.from,
@@ -55,7 +84,7 @@ export default class Page {
             formatHeading: data => `$${data}`
           });
 
-        const chartCustomers = new ColumnChart({
+        this.chartCustomers = new ColumnChart({
             url: 'api/dashboard/customers',
             range: {
               from: this.dates.from,
@@ -63,36 +92,19 @@ export default class Page {
             },
             label: 'customers',
           });
-
-        wrapper.querySelector('.content__top-panel').append(rangePicker.element);
-        wrapper.querySelector('.dashboard__charts').append(chartOrders.element);
-        wrapper.querySelector('.dashboard__charts').append(chartSales.element);
-        wrapper.querySelector('.dashboard__charts').append(chartCustomers.element);
-        wrapper.append(sortableTable.element);
-
-        
-
-        this.element = wrapper;
-        this.subElements = {
-            rangePicker: rangePicker.element,
-            sortableTable: sortableTable.element,
-            ordersChart: chartOrders.element,
-            customersChart: chartCustomers.element,
-            salesChart: chartSales.element 
-        }
-
-        wrapper.remove();
-
-        this.initEventListeners();
-
-        return this.element;
     }
 
     initEventListeners() {
-        this.element.addEventListener('date-select', (evt) => {
+        this.element.addEventListener('date-select', async (evt) => {
             this.dates.from = evt.detail.from;
             this.dates.to = evt.detail.to;
-            this.render();
+
+            await this.chartOrders.update(this.dates.from, this.dates.to);
+            await this.chartCustomers.update(this.dates.from, this.dates.to);
+            await this.chartSales.update(this.dates.from, this.dates.to);
+
+            this.sortableTable.url = new URL(`api/dashboard/bestsellers?from=${this.dates.from.toISOString()}&to=${this.dates.to.toISOString()}`, BACKEND_URL);
+            console.log(this.sortableTable);
         });
     }
 
